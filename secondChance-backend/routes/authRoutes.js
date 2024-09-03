933e30ecb5c3ab880c0d27f25639cf3f37371ee5
stergios nanos <35434161+stergion@users.noreply.github.com>
2024-09-03 13:53:21 -0400

    Implement user update endpoint

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const connectToDatabase = require('../models/db');
 const logger = require('../logger');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { body, validationResult, header } = require('express-validator');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -61,15 +62,15 @@ router.post('/login', async (req, res) => {
         const user = await collection.findOne({ "email": req.body.email });
 
         if (!user) {
-            logger.error('User not found')
+            logger.error('User not found');
             return res.status(404).json({ error: 'User not found' });
         }
 
         if (!bcrypt.compareSync(req.body.password, user.password)) {
-            logger.error('Wrong pasword')
+            logger.error('Wrong pasword');
             return res.status(404).json({ error: 'Wrong pasword' });
         }
-        
+
         const payload = {
             user: {
                 id: user._id.toString()
@@ -77,11 +78,64 @@ router.post('/login', async (req, res) => {
         };
         const authtoken = jwt.sign(payload, process.env.JWT_SECRET);
 
-        res.json({ authtoken, userName:user.userName, email:user.email });
+        res.json({ authtoken, userName: user.userName, email: user.email });
     } catch (e) {
         logger.error(e);
         return res.status(500).send('Internal server error');
 
+    }
+});
+
+router.put('/update', async (req, res) => {
+    logger.info('put /auth/update called')
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const email = req.headers.email;
+    if (!email) {
+        logger.error('Email not found in the request headers');
+        return res.status(400).json({ error: "Email not found in the request headers" });
+    }
+
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const user = await collection.findOne({ "email": email });
+
+        if (!user) {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        for (const key in req.body) {
+            if (Object.hasOwnProperty.call(req.body, key)) {
+                user[key] = req.body[key];
+            }
+        }
+        user.updatedAt = new Date();
+
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: user },
+            { returnDocument: 'after' }
+        );
+
+        const payload = {
+            user: {
+                id: updatedUser._id.toString()
+            }
+        };
+        const authtoken = jwt.sign(payload, process.env.JWT_SECRET);
+
+        res.json({ authtoken });
+    } catch (e) {
+        logger.error(e);
+
+        return res.status(500).send('Internal server error');
     }
 });
 
