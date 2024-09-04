b2cee426ca83f37c87bec8fe85101460aa6a6c81
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-09-04 04:11:05 +0300

    appease linter

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -3,8 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
-let filename = `${__dirname}/secondChanceItems.json`;
+const url = `${process.env.MONGO_URL}`;
+const filename = `${__dirname}/secondChanceItems.json`;
 const dbName = 'secondChance';
 const collectionName = 'secondChanceItems';
 
@@ -12,39 +12,39 @@ const collectionName = 'secondChanceItems';
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs;
 
 // connect to database and insert data into the collection
-async function loadData() {
-    const client = new MongoClient(url);
+async function loadData () {
+  const client = new MongoClient(url);
 
-    try {
-        // Connect to the MongoDB client
-        await client.connect();
-        console.log("Connected successfully to server");
+  try {
+    // Connect to the MongoDB client
+    await client.connect();
+    console.log('Connected successfully to server');
 
-        // database will be created if it does not exist
-        const db = client.db(dbName);
+    // database will be created if it does not exist
+    const db = client.db(dbName);
 
-        // collection will be created if it does not exist
-        const collection = db.collection(collectionName);
-        let cursor = await collection.find({});
-        let documents = await cursor.toArray();
+    // collection will be created if it does not exist
+    const collection = db.collection(collectionName);
+    const cursor = await collection.find({});
+    const documents = await cursor.toArray();
 
-        if(documents.length == 0) {
-            // Insert data into the collection
-            const insertResult = await collection.insertMany(data);
-            console.log('Inserted documents:', insertResult.insertedCount);
-        } else {
-            console.log("Items already exists in DB")
-        }
-    } catch (err) {
-        console.error(err);
-    } finally {
-        // Close the connection
-        await client.close();
+    if (documents.length == 0) {
+      // Insert data into the collection
+      const insertResult = await collection.insertMany(data);
+      console.log('Inserted documents:', insertResult.insertedCount);
+    } else {
+      console.log('Items already exists in DB');
     }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Close the connection
+    await client.close();
+  }
 }
 
 loadData();
 
 module.exports = {
-    loadData,
-  };
+  loadData
+};
