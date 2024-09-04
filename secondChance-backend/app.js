f09012b8f057281cad7a0e185a367682bbd1562b
Stergios Nanos <35434161+stergion@users.noreply.github.com>
2024-09-04 03:58:27 +0300

    appease linter

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -12,17 +12,15 @@ const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+  pinoLogger.info('Connected to DB');
+}).catch((e) => console.error('Failed to connect to DB', e));
 
 app.use(express.json());
 
 // Route files
-const secondChanceItemsroutes = require('./routes/secondChanceItemsRoutes')
-const searchRoutes = require('./routes/searchRoutes')
-const authRoutes = require('./routes/authRoutes')
+const secondChanceItemsroutes = require('./routes/secondChanceItemsRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -30,20 +28,20 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-app.use('/api/secondchance/items', secondChanceItemsroutes)
-app.use('/api/secondchance/search', searchRoutes)
+app.use('/api/secondchance/items', secondChanceItemsroutes);
+app.use('/api/secondchance/search', searchRoutes);
 app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  console.error(err);
+  res.status(500).send('Internal Server Error');
 });
 
 app.get('/', (req, res) => {
-    res.send('Inside the server')
-})
+  res.send('Inside the server');
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
